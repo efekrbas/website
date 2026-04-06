01a9d5cbc5bd2a60cc7c2a183dc55ec97b27e64b
Efe Kırbaş <efekrbass@gmail.com>
2026-04-06 07:47:38 +0300

    Trigger fresh Vercel deployment for environment variable sync

--- api/spotify.js
+++ api/spotify.js
@@ -1,3 +1,4 @@
+// Triggering a fresh deployment to force Vercel environment variable synchronization
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 const NOW_PLAYING_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing';
 
